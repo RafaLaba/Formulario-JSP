b1358ff41b3ec9b18329a12fee9dc61bd55cd058
Rafael Laba <rafaellaba1@gmail.com>
2019-04-01 15:10:43 -0300

    conexao db

---
+++ FormularioServlets/src/br/com/trabalho/DB/DBconection.java
@@ -0,0 +1,27 @@
+package br.com.trabalho.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconection {
+	public static void main(String[] args){
+		
+		Connection conexao = null;
+		try {
+			//Registro da classe JDBC e os parametros de conexÃ£o do banco
+			String url = "jdbc:mysql://localhost/formualrio";
+			String usuario = "root";
+			String senha = "";
+			conexao = DriverManager.getConnection(url,usuario,senha);
+			System.out.println("Conectou!");
+			conexao.close();
+		} catch(SQLException e) {
+			System.out.println("Ocorreu um erro ao criar a conexÃ£o: Erro: " + e.getMessage());
+		}
+
+	}
+
+	
+	
+}
